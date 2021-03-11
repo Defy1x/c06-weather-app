9c4def07e9331592e9530aba3d6a094907004f21
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-03-10 21:50:17 -0500

    basic js working

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,74 @@ $(document).ready(function() {
   for(i = list.length; i--; )
   icons.set(list[i], list[i]);
   icons.play();
+
+$('#search-form').submit(function(e){
+    e.preventDefault()
+})
+
+  // grab value from form
+$('#search-btn').click((event) =>{
+  event.preventDefault()
+  city = $('#city-name').val().toLowerCase()
+  console.log(city)
+
+// if no value, exit
+  if (!city){
+  return;
+  }
+
+// parameter for call
+  search(city)
+
+//clear form
+  $('#city-name').val('')
+  })
 });
 
-//load in today's date"
-var todayDate = moment().format("dddd, MMMM Do");
-$("#current-date").text(todayDate);
+// async function begins
+async function search(city) {
+
+  try {
+
+    const url = 'https://api.openweathermap.org/data/2.5/weather'
+    const apiKey = '4159a716c99e2d3e9c669ff6a22c35db'
+
+    let response = await axios.get(url, {
+        params: {
+        q: city,
+        appid: apiKey
+        }
+      })
+      console.log(response.data)
+      displayResults(response.data)
+
+    } catch (error) {
+      console.log(error)
+      alert('Please enter a valid city')
+    }
+  }
+
+  function displayResults(weatherData) {
+
+      $('#city').text(`${weatherData.name},${weatherData.sys.country}`)
+      let status = `${weatherData.weather[0].description}`
+      $('#today-status').text(status)
+      getDate()
+
+      function convertToF(kelvin) {
+      let celcius = kelvin - 273
+      let farenheit = celcius * (9/5) + 32
+      return Math.floor(farenheit)
+      }
+
+      const currentTemp = convertToF(weatherData.main.temp)
+      console.log(currentTemp)
+
+      $('#today-temp').html(`${currentTemp}°F`)
+  }
+
+// pull in today's date
+  function getDate(){
+    var todayDate = moment().format("dddd, MMMM Do");
+    $("#current-date").text(todayDate);
+  }
